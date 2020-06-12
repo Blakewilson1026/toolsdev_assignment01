a401e738cb2963a2bf79d74592e0d9b7ef552134
Blake <blakewilson@legalshieldcorp.com>
2020-06-12 09:22:27 -0500

    Got download punkt to work, stuck on figuring out nlp

--- gathernews.py
+++ gathernews.py
@@ -1,19 +1,39 @@
 import nltk
+import ssl
+import newspaper
 from newspaper import Article
 
-#find article urls
-articleURLs = ['https://www.gamespot.com/articles/heres-what-ps5-looks-like-playstation-5-design-rev/1100-6478306/', 
-               'https://www.gamespot.com/articles/spiderman-miles-morales-announced-for-ps5/1100-6478314/',
-               'https://www.gamespot.com/articles/call-of-duty-warzone-modern-warfare-patch-notes-fo/1100-6478276/']
-articles = []
+#functions
+def downloadPunkt():
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+    except AttributeError:
+        pass
+    else:
+        ssl._create_default_https_context = _create_unverified_https_context
+    nltk.download('punkt')
 
-#create list of articles
-for articleURL in articleURLs:
-    articles.append(Article(articleURL))
+# find news site urls
+newsSites =  [newspaper.build('https://www.gamespot.com/news/' , memorize_articles=False),
+              newspaper.build('https://www.pcgamer.com/news/' , memorize_articles=False),
+              newspaper.build('https://www.gamesradar.com/news/' , memorize_articles=False)]
+#gameSpot_News = newspaper.build('https://www.gamespot.com/news/' , memorize_articles=False)
 
-#NLP 
-nltk.download('punkt')
-for article in articles:
-    article.download()
-    article.parse()
-    article.nlp()
+# NLP 
+downloadPunkt()
+for newsSite in newsSites:
+    for article in newsSite.articles:
+        article.download()
+        article.parse()
+        article.nlp()
+
+# ask users to enter keyword
+print("Enter a keyword and press ENTER to search: ")
+keyword = input()
+
+# test output
+if keyword in article.keywords:
+    print(article.authors)
+    
+# open file to write to
+#newsSummary  = open('news_summary.txt', 'w')
