28feecc6fdfd9f26662b8a8c3e316ade4730c1f1
Blake <blakewilson@legalshieldcorp.com>
2020-06-12 10:23:04 -0500

    output working, issue was due to incompatible news sites

--- gathernews.py
+++ gathernews.py
@@ -14,10 +14,23 @@ def downloadPunkt():
     nltk.download('punkt')
 
 # find news site urls
-newsSites =  [newspaper.build('https://www.gamespot.com/news/' , memorize_articles=False),
-              newspaper.build('https://www.pcgamer.com/news/' , memorize_articles=False),
-              newspaper.build('https://www.gamesradar.com/news/' , memorize_articles=False)]
-#gameSpot_News = newspaper.build('https://www.gamespot.com/news/' , memorize_articles=False)
+newsSites =  [newspaper.build('https://www.nytimes.com/' , memorize_articles=False),
+              newspaper.build('https://www.foxnews.com/' , memorize_articles=False),
+              newspaper.build('https://www.cnn.com/' , memorize_articles=False)]
+#nyTimes_News = newspaper.build('https://www.nytimes.com/' , memorize_articles=False)
+
+# ask users to enter keyword
+print("Enter a keyword and press ENTER to search: ")
+keyword = input()
+
+# test nlp
+#downloadPunkt()
+#for article in nyTimes_News.articles:
+#    print(article)
+#    article.download()
+#    article.parse()
+#    article.nlp()
+#    print(article.authors)
 
 # NLP 
 downloadPunkt()
@@ -27,13 +40,6 @@ for newsSite in newsSites:
         article.parse()
         article.nlp()
 
-# ask users to enter keyword
-print("Enter a keyword and press ENTER to search: ")
-keyword = input()
-
-# test output
-if keyword in article.keywords:
-    print(article.authors)
     
 # open file to write to
 #newsSummary  = open('news_summary.txt', 'w')
