dd44fd478346d6a08c8443fd58223cf6245494fd
Blake <blakewilson@legalshieldcorp.com>
2020-06-12 12:33:36 -0500

    Completed working assignment. added functionality to let the user add a news site

--- gathernews.py
+++ gathernews.py
@@ -16,35 +16,45 @@ def printArticleInfo(article):
     print(str(article.title) + " -- " + str(article.authors))
     print(article.summary)
     print(" ")
+def writeArticleInfoToFile(article,file):
+    file.writelines(str(article.title) + " -- " + str(article.authors) + '\n')
+    file.writelines(article.summary + '\n')
+    file.writelines('\n')
 
 # find news site urls
 newsSites =  [newspaper.build('https://www.nytimes.com' , memoize_articles=False),
               newspaper.build('https://www.nbcnews.com/' , memoize_articles=False),
-              newspaper.build('https://www.cnn.com' , memoize_articles=False)]
+              newspaper.build('https://www.washingtonpost.com/' , memoize_articles=False)]
 #nyTimes_News = newspaper.build('https://www.nytimes.com/' , memorize_articles=False)
+
+# ask users to provide news site
+print("Would you like to add a news source? (y/n)")
+answer = input()
+if answer == 'Y':
+    answer = 'y'
+if answer == 'y':
+    print("Enter news URL: ")
+    newNewsSite = input()
+    newsSites.append(newspaper.build(newNewsSite , memoize_articles=False))
+
 
 # ask users to enter keyword
 print("Enter a keyword and press ENTER to search: ")
 keyword = input()
 
-# test nlp
-#downloadPunkt()
-#for article in nyTimes_News.articles:
-#    print(article)
-#    article.download()
-#    article.parse()
-#    article.nlp()
-#    print(article.authors)
-
-# NLP 
+# NLP and writing to file
 downloadPunkt()
+newsSummary = open('news_summary.txt' , 'w')
 for newsSite in newsSites:
     for article in newsSite.articles:
         article.download()
         article.parse()
         article.nlp()
-        if keyword in article.keywords:
+        if keyword == "":
             printArticleInfo(article)
-
-# open file to write to
-#newsSummary  = open('news_summary.txt', 'w')
+            writeArticleInfoToFile(article, newsSummary)
+        else:
+            if keyword in article.keywords:
+                printArticleInfo(article)
+                writeArticleInfoToFile(article,newsSummary)
+newsSummary.close()
